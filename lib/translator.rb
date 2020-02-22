1bcc4151ebaf97fcd9538bbb4fbafb683b0c08f4
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-22 04:43:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,10 @@ require 'pry'
 
 def load_library(file_path)
   emote_list = YAML.load_file(file_path)
+  binding.pry
   meanings = []
   
-  {:get_meaning => nil, :get_emoticon => nil}
+  result = {:get_meaning => nil, :get_emoticon => nil}
 end
 
 def get_japanese_emoticon
